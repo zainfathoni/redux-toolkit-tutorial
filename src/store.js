3e5de82d0f9fbfa78e60c5ac4c767f8142821fb7
Zain Fathoni <me@zainf.dev>
2023-07-06 21:22:19 +0800

    feat(store): add localStorage integration

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counterSlice";
+import { loadState, saveState } from "./localStorage";
+
+const persistedState = loadState();
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  preloadedState: persistedState,
+});
+
+store.subscribe(() => {
+  saveState({
+    counter: store.getState().counter,
+  });
 });
 
 export default store;
